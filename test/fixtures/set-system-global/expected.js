0f78682d1ac4455b0779cecea777baff25941c11
Jochen Rauschenbusch <jochen.rauschenbusch@bridging-it.de>
2016-07-26 13:13:38 +0200

    Unit tests

---
+++ test/fixtures/set-system-global/expected.js
@@ -0,0 +1,6 @@
+foo.registerDynamic([], true, function ($__require, exports, module) {
+  var define,
+      global = this,
+      GLOBAL = this;
+  return module.exports;
+});
---
+++ test/fixtures/use-optimize/expected.js
@@ -0,0 +1,7 @@
+System.registerDynamic([], true, function ($__require, exports, module) {
+  var define,
+      global = this,
+      GLOBAL = this;
+  "production";
+  return module.exports;
+});
